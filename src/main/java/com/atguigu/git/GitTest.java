a4078d46d99dc1dbeecb0e4f76bfac6d8ac0cd74
Thebigpotato <1225913685@qq.com>
2025-02-26 16:04:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.atguigu.git
+ * Description:
+ *
+ * @Author the big potato
+ * @Create 2025/2/26 15:58
+ * @Version 19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
